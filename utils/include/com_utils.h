54eb64c23b00d044b0af6a58a8a793140293ee09
git <1079843691@qq.com>
2020-07-03 16:02:04 +0800

    1.实现一个小函数，获取本机的mac地址

---
+++ utils/include/com_utils.h
@@ -0,0 +1,29 @@
+#ifndef _COM_UTILS_H_
+#define _COM_UTILS_H_
+
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <iostream>
+#include <vector>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<net/if.h>
+#include<net/if_arp.h>
+#include<arpa/inet.h>
+#include<sys/ioctl.h>
+
+
+#endif
--- utils/src/main.cpp
+++ utils/src/main.cpp
@@ -1,3 +1,7 @@
+/********************************************************************************************
+* Brief：
+********************************************************************************************/
+#include "com_utils.h"
 #include "stl_tools.h"
 #include "print.h"
 
@@ -12,10 +16,31 @@ int test_STLTest(){
 	return 0;
 }
 
+int get_local_mac(char *mac, int len_limit){
+    struct ifreq mifreq;
+    int sock = 0;
+	if ((sock = socket (AF_INET, SOCK_STREAM, 0)) < 0){
+        printf("[AIR][Er][%s][%d]:info=%s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+    strcpy (mifreq.ifr_name, "eth0");
+    if (ioctl (sock, SIOCGIFHWADDR, &mifreq) < 0){
+        printf("[AIR][Er][%s][%d]:info=%s\n", __func__, __LINE__, strerror(errno));
+        return -1;
+    }
+	snprintf (mac, len_limit, "%X:%X:%X:%X:%X:%X", (unsigned char) mifreq.ifr_hwaddr.sa_data[0], 
+	(unsigned char) mifreq.ifr_hwaddr.sa_data[1], (unsigned char) mifreq.ifr_hwaddr.sa_data[2], (unsigned char)
+	mifreq.ifr_hwaddr.sa_data[3], (unsigned char) mifreq.ifr_hwaddr.sa_data[4], (unsigned char)mifreq.ifr_hwaddr.sa_data[5]);
+	printf("[AIR][%s][%d]:mac=%s\n", __func__, __LINE__, mac);
+	return 0;
+}
+
 int main(int argc, const char *argv[])
 {
-	test_STLTest();
-	test_myString();
+	char mac_addr[32] = {0};
+	get_local_mac(mac_addr, sizeof(mac_addr));
+	//test_STLTest();
+	//test_myString();
 	return 0;
 }
 
