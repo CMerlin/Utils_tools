979ee565d9a976050f4e603daaff94ef1395a831
chenzhongke <chenzhongrui@insta360.com>
2020-09-18 18:20:10 +0800

    1.上传一个队列的测试demo

--- Demo_c++/src/rtc_queue.cpp
+++ Demo_c++/src/rtc_queue.cpp
@@ -9,7 +9,7 @@ extern "C" {
 
 #include "rtc_queue.h"
 
-unsigned char TAG[32] = {"[ME][sip_stream_sink.c]"};
+unsigned char TAG[32] = {"[ME][rtc_queue.c]"};
 //char DTA[32] = {"hhh"};
 
 #if RTC_QUEUE /* RTC_QUEUE ****************************************************************/
@@ -94,23 +94,41 @@ Queue *rtc_queue_a = NULL;
 pthread_mutex_t mtx_v;
 pthread_mutex_t mtx_a;
 
+int init_av_queue(){
+	rtc_queue_v = (Queue*)malloc(sizeof(Queue));
+	rtc_queue_a = (Queue*)malloc(sizeof(Queue));
+	memset(rtc_queue_v, 0, sizeof(Queue));
+	memset(rtc_queue_a, 0, sizeof(Queue));
+	pthread_mutex_init (&mtx_v, NULL);
+	pthread_mutex_init (&mtx_a, NULL);
+	//pthread_mutex_lock(&mutex);
+	//pthread_mutex_unlock(&mutex);
+	//record_init();
+	return 0;
+}
+
 int release_rtc_queue(){
 	int countv = QueueSize(rtc_queue_v);
 	int counta = QueueSize(rtc_queue_a);
 	LOGINFO("[%s][%s][%d]:countv=%d counta=%d\n", TAG, __func__, __LINE__, countv, counta);
+	LOGINFO("[%s][%s][%d]:\n", TAG, __func__, __LINE__);
 
 	while(1){
 		//if((countv<=0) && (countv<=0)){
+		LOGINFO("[%s][%s][%d]:\n", TAG, __func__, __LINE__);
 		if((0 == QueueEmpty(rtc_queue_v)) && (0 == QueueEmpty(rtc_queue_a))){
 			break;
 		}
+		LOGINFO("[%s][%s][%d]:\n", TAG, __func__, __LINE__);
 		if(0 != QueueEmpty(rtc_queue_v)){
 			QueuePop(rtc_queue_v);
 		}
-
+		LOGINFO("[%s][%s][%d]:\n", TAG, __func__, __LINE__);
 		if(0 != QueueEmpty(rtc_queue_a)){
 			QueuePop(rtc_queue_a);
 		}
+		LOGINFO("[%s][%s][%d]:\n", TAG, __func__, __LINE__);
+		usleep(1000 * 1000);
 	}
 	LOGINFO("[%s][%s][%d]:exit countv=%d counta=%d\n", TAG, __func__, __LINE__, countv, counta);
 	return 0;
@@ -121,6 +139,22 @@ int write_video_to_sip(S_VIDEO_PKG *frame){
 		LOGERR("[Er][%s][%s][%d]:data_len=%d\n", TAG, __func__, __LINE__, (frame->data_len));
 		return -1;
 	}
+
+	#if 0
+		pthread_mutex_lock(&mtx_v);
+		QDataType data;
+		memset(&data, 0, sizeof(QDataType));
+		data.data = (unsigned char*)malloc((frame->data_len));
+		memset(data.data, 0, sizeof(frame->data_len));
+		data.type = EAV_VIDEO;
+		data.pts = frame->pts;
+		memcpy(data.data, (frame->data), (frame->data_len));
+		QueuePush(rtc_queue_v, data);
+		pthread_mutex_unlock(&mtx_v);
+	#endif
+
+
+	#if 1
 	pthread_mutex_lock(&mtx_v);
 	QDataType *data = (QDataType*)malloc(sizeof(QDataType));
 	memset(data, 0, sizeof(QDataType));
@@ -131,6 +165,7 @@ int write_video_to_sip(S_VIDEO_PKG *frame){
 	memcpy(data->data, (frame->data), (frame->data_len));
 	QueuePush(rtc_queue_v, (*data));
 	pthread_mutex_unlock(&mtx_v);
+	#endif
 
 	return 0;
 }
@@ -138,10 +173,35 @@ int write_video_to_sip(S_VIDEO_PKG *frame){
 static void *thread_rtc_task(void *arg)
 {
 	LOGINFO("[KK][%s][%s][%d]:runing\n", TAG, __func__, __LINE__);
+	int count = 0;
+	init_av_queue();
 	while(1){
+		usleep(1 * 1000 * 1000);
+		count++;
 		LOGINFO("[ME][%s][%s][%d]:\n", TAG, __func__, __LINE__);
-		//release_rtc_queue();
-		usleep(10 * 1000 * 1000);
+
+		#if 1 /* -------------------------------- */
+		S_VIDEO_PKG pkg;
+		unsigned char buffer[2048] = {0};
+		pkg.data = buffer;
+		pkg.data_len = 2048;
+		pkg.pts = time(NULL);
+		write_video_to_sip(&pkg);
+		#endif
+		
+		#if 0 /* -------------------------------- */
+		S_VIDEO_PKG pkg;
+		unsigned char buffer[2048] = {0};
+		pkg.data = buffer;
+		pkg.data_len = 2048;
+		pkg.pts = time(NULL);
+		write_video_to_sip(&pkg);
+		#endif
+		/* -------------------------------- */
+		if(10 <= count){
+			count = 0;
+			release_rtc_queue();
+		}
 	}
 	LOGINFO("[%s][%s][%d]:exit\n", TAG, __func__, __LINE__);
 	return NULL;
@@ -160,6 +220,10 @@ int init_rtc_task(){
 
 int test_func_queue(){
 	init_rtc_task();
+	while(1){
+		usleep(10 * 1000 * 1000);
+	}
+	return 0;
 }
 
 #ifdef __cplusplus
